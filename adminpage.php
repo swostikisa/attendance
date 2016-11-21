79c2ab1f73bcd89d9761ea81bc68c5f0cdf2ffd0
Swostik Maharjan <spwostik@gmail.com>
2016-11-21 12:37:38 +0545

    look

--- adminpage.php
+++ adminpage.php
@@ -4,7 +4,7 @@
 	<h1>Admin Main Page</h1>
 	</head>
 	<p><a href="file:///C:/xampp/htdocs/attandance/admin/adminpage.html">Change Password<a></p>
-	<p><a href="file:///C:/xampp/htdocs/attandance/admin/add.html">Add Employee<a></p>
+	<p><a href="file:///C:/xampp/htdocs/Attain/attendance/add.html">Add Employee<a></p>
 	<p><a href="file:///C:/xampp/htdocs/attandance/admin/adminpage.html">Remove Employee</a></p>
 	
 <body>
