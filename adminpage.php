8f7f7f78e1f0092658976a67747e258bdb790117
Swostik Maharjan <spwostik@gmail.com>
2016-11-21 13:58:13 +0545

    also

--- adminpage.php
+++ adminpage.php
@@ -3,10 +3,13 @@
 <head>
 	<h1>Admin Main Page</h1>
 	</head>
+	
+<body>
+	Welcome <?php echo $_POST["name"]; ?><br>
 	<p><a href="file:///C:/xampp/htdocs/attandance/admin/adminpage.html">Change Password<a></p>
 	<p><a href="file:///C:/xampp/htdocs/Attain/attendance/add.html">Add Employee<a></p>
 	<p><a href="file:///C:/xampp/htdocs/attandance/admin/adminpage.html">Remove Employee</a></p>
 	
-<body>
+
 	</body>
 </html>
